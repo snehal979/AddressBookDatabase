fa5267b3386bcd2c459e5077c099ac8ce99f489a
Snehal979 <bansodsnehal535@gmail.com>
2023-03-16 09:01:18 +0530

    [Snehal]Add:Create table for AddressBook

--- AddressBookDatabase.sql
+++ AddressBookDatabase.sql
@@ -1,2 +1,14 @@
 --Uc1 Create database
 Create database AddressBookServiceDatabase
+
+--UC2 create table
+CREATE TABLE AddressBookTable(
+firstname VARCHAR(20) NOT NULL,
+lastnames VARCHAR(20)NOT NULL,
+address VARCHAR(50),
+city VARCHAR(15)NOT NULL,
+state VARCHAR(15)NOT NULL,
+zip VARCHAR(6),
+phonenumber VARCHAR(10),
+email VARCHAR(20)NOT NULL
+);
